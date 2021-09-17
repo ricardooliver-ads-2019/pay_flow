fd275f94db401d5ad225071d0b6f3636d780db07
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2021-09-17 06:32:36 -0300

    Update insert_boleto_page.dart

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
